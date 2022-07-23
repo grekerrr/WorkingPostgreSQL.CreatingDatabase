20936aa113b578c44abedf288f05ec7fb105ec76
Dmitry Greker <bestnd@mail.ru>
2022-07-23 14:38:17 +0300

    new files

--- РаботаPostgreSQL.СозданиеБДWorkers.sql
+++ РаботаPostgreSQL.СозданиеБДWorkers.sql
@@ -1,12 +1,6 @@
-CREATE TABLE IF NOT EXISTS Departments (
-   DepartmentID SERIAL PRIMARY KEY,
-   DepartmentName TEXT NOT NULL,
-   DirectorName TEXT NOT NULL
-)
-
 CREATE TABLE IF NOT EXISTS Workers (
    WorkerID SERIAL PRIMARY KEY,
    WorkerName TEXT NOT NULL,
-   DirectorName TEXT NOT NULL,
-   Department_ID INTEGER NOT NULL REFERENCES Departments(DepartmentID)
+   DepartmentName TEXT NOT NULL,
+   Director_ID INTEGER REFERENCES Workers(WorkerID)
 )
